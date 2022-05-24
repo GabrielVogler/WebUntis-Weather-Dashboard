c6d8fd94c8c517fdf7cb7d7ee378ccdfe42bd3bd
FilipIlic5k <77800839+FilipIlic5k@users.noreply.github.com>
2022-05-24 13:22:24 +0200

    added WeatherTestClass

--- dashboard/src/com/voglic/backend/Weather.java
+++ dashboard/src/com/voglic/backend/Weather.java
@@ -3,33 +3,10 @@ package com.voglic.backend;
 import net.aksingh.owmjapis.DailyForecast;
 import net.aksingh.owmjapis.OpenWeatherMap;
 import org.json.JSONException;
-
 import java.io.IOException;
 
 public class Weather {
     public static void main(String[] args) {
-        boolean isMetric = true;
-        String owmApiKey = "XXXXXXXXXXXX"; /* YOUR OWM API KEY HERE */
-        String weatherCity = "Brisbane,AU";
-        byte forecastDays = 3;
-        OpenWeatherMap.Units units = (isMetric)
-                ? OpenWeatherMap.Units.METRIC
-                : OpenWeatherMap.Units.IMPERIAL;
-        OpenWeatherMap owm = new OpenWeatherMap(units, owmApiKey);
-        try {
-            DailyForecast forecast = owm.dailyForecastByCityName(weatherCity, forecastDays);
-            System.out.println("Weather for: " + forecast.getCityInstance().getCityName());
-            int numForecasts = forecast.getForecastCount();
-            for (int i = 0; i < numForecasts; i++) {
-                DailyForecast.Forecast dayForecast = forecast.getForecastInstance(i);
-                DailyForecast.Forecast.Temperature temperature = dayForecast.getTemperatureInstance();
-                System.out.println("\t" + dayForecast.getDateTime());
-                System.out.println("\tTemperature: " + temperature.getMinimumTemperature() +
-                        " to " + temperature.getMaximumTemperature() + "\n");
-            }
-        }
-        catch (IOException | JSONException e) {
-            e.printStackTrace();
-        }
+       WeatherTest.main();
     }
 }
