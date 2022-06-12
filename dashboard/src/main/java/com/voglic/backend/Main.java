8eb69b74237ac4d303941872228370a5f2b53bd1
FilipIlic5k <77800839+FilipIlic5k@users.noreply.github.com>
2022-06-12 15:14:28 +0200

    Gui: Added static Weather and Untis Design

--- dashboard/src/main/java/com/voglic/backend/Main.java
+++ dashboard/src/main/java/com/voglic/backend/Main.java
@@ -7,17 +7,17 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //Test Subject
-        /*Subject am = new Subject("../files/JSON_Stunden/timetable.json", 0);
+        Subject am = new Subject("../files/JSON_Stunden/timetable.json", 0);
         System.out.println(am.name);
         System.out.println(am.className);
         System.out.println(am.teacher);
         System.out.println(am.room);
         System.out.println(am.starttime);
         System.out.println(am.endtime);
-        System.out.println(am.color);*/
-        //System.out.println(new Subject());
-        //SchoolDay day = new SchoolDay("../files/JSON_Stunden/timetable.json");
-        //System.out.println(day);
+        System.out.println(am.color);
+        System.out.println(new Subject());
+        SchoolDay day = new SchoolDay("../files/JSON_Stunden/timetable.json");
+        System.out.println(day);
         /*WebUntisApi.run();
         day = new SchoolDay("../files/JSON_Stunden/timetable.json");
         System.out.println(day);*/
